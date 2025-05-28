8a52559850fac91df5b0552f5d8baa402d0946d3
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-05-28 16:00:51 -0300

    feat:auth_guards_fix
    auth guard fixed
    
    Kaique A Mesquita

--- frontend/Estoque_WebApp/src/app/app.routes.ts
+++ frontend/Estoque_WebApp/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { ManageLayoutComponent } from './shared/layouts/manage-layout/manage-layout.component';
+import { authGuard } from './core/guards/auth.guard';
+
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'manage',component: ManageLayoutComponent},
-    {path: 'login', component: LoginComponent },
+
+        
+    { path: 'login', component: LoginComponent },
+    { path: 'manage', component: ManageLayoutComponent, canActivate: [authGuard] },
     
 
 ];
