651c00e7270f0d88897d366f183644e69fa2dbc8
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-06-05 17:30:50 -0300

    feat:last_movements_table
    last movements table created
    
    Kaique A Mesquita

--- frontend/Estoque_WebApp/src/app/pages/dashboard/dashboard.component.ts
+++ frontend/Estoque_WebApp/src/app/pages/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import { DashboardCardsComponent } from '../../shared/components/dashboard/dashb
 import { DashboardActionsComponent } from '../../shared/components/dashboard/dashboard-actions/dashboard-actions.component';
 import { PTableComponent } from '../../shared/components/p-table/p-table.component';
 import { ExpirationBatches } from '../../shared/models/expiration-batches';
+import { LastMovements } from '../../shared/models/last-movements';
 
 @Component({
   selector: 'app-dashboard',
@@ -69,6 +70,48 @@ export class DashboardComponent {
       remainingDays: 150,
       quantity: 120,
       containerCode: 'C104'
+    }
+  ];
+  lastMovements: LastMovements[] = [
+    {
+      date: new Date('2023-10-01T10:00:00'),
+      type: 'Entrada',
+      productName: 'Produto A',
+      quantity: 50,
+      origin_destination: 'Estoque',
+      userName: 'Usuário 1'
+    },
+    {
+      date: new Date('2023-10-02T11:30:00'),
+      type: 'Saída',
+      productName: 'Produto B',
+      quantity: 20,
+      origin_destination: 'Cozinha',
+      userName: 'Usuário 2'
+    },
+    {
+      date: new Date('2023-10-03T14:15:00'),
+      type: 'Entrada',
+      productName: 'Produto C',
+      quantity: 30,
+      origin_destination: 'Estoque',
+      userName: 'Usuário 3'
+    },
+    {
+      date: new Date('2023-10-04T09:45:00'),
+      type: 'Saída',
+      productName: 'Produto D',
+      quantity: 10,
+      origin_destination: 'Cozinha',
+      userName: 'Usuário 4'
+    },
+    {
+      date: new Date('2023-10-05T16:20:00'),
+      type: 'Entrada',
+      productName: 'Produto E',
+      quantity: 100,
+      origin_destination: 'Estoque',
+      userName: 'Usuário 5'
     }
   ];
 
