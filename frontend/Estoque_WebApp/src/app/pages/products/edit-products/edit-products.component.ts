dfcfe21d4cb45bdb28fcb2c498689c43f61288a4
JRLSF <jlisboa600@gmail.com>
2025-07-15 23:33:39 -0300

    Create Products Component

---
+++ frontend/Estoque_WebApp/src/app/pages/products/edit-products/edit-products.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-products',
+  imports: [],
+  templateUrl: './edit-products.component.html',
+  styleUrl: './edit-products.component.css'
+})
+export class EditProductsComponent {
+
+}
