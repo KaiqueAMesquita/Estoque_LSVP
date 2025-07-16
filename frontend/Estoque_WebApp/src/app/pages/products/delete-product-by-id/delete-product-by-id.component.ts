48bb99290cba851332df08e7c10b89f88ebb959c
JRLSF <jlisboa600@gmail.com>
2025-07-15 23:49:55 -0300

    Created Products Update Method

---
+++ frontend/Estoque_WebApp/src/app/pages/products/delete-product-by-id/delete-product-by-id.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-product-by-id',
+  imports: [],
+  templateUrl: './delete-product-by-id.component.html',
+  styleUrl: './delete-product-by-id.component.css'
+})
+export class DeleteProductByIdComponent {
+
+}
