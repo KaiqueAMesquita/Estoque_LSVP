84f212fdf17980e0a675bb80e19c2a023673debc
JRLSF <jlisboa600@gmail.com>
2025-10-22 01:14:34 -0300

    Routes BugFix Added Movements Ways

--- frontend/Estoque_WebApp/src/app/app.routes.ts
+++ frontend/Estoque_WebApp/src/app/app.routes.ts
@@ -14,7 +14,7 @@ import { CreateProductsComponent } from './pages/products/create-products/create
 import { CreateContainerComponent } from './pages/container/create-container/create-container.component';
 import { ViewContainersComponent } from './pages/container/view-containers/view-containers.component';
 import { The404PageComponent } from './shared/components/the404-page/the404-page.component';
-import { TesteComponent } from './teste/teste.component';
+//import { TesteComponent } from './teste/teste.component';
 import { UnitInputComponent } from './pages/unit/unit-input/unit-input.component';
 import { EditContainerComponent } from './pages/container/edit-container/edit-container.component';
 import { CreateCategoryComponent } from './pages/category/create-category/create-category.component';
