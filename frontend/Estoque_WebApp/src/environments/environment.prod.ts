37c16713bb21d47c7ffda8f541e0d7341bfddd7b
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-11-14 11:36:31 -0300

    feat:fix_errors

--- frontend/Estoque_WebApp/src/environments/environment.prod.ts
+++ frontend/Estoque_WebApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,  
-    API_URL: "http://localhost:8080/api",
+    API_URL: "http://192.168.1.75:8080/api",
     TOKEN_KEY: "Centro_Paula_Souza_Token"
 };
