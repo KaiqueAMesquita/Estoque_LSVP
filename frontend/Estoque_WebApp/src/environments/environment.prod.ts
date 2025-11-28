0a213275b3510f165aba1413b3050ba92074ec98
KaiqueAMesquita <kaiquealvesmesquita@gmail.com>
2025-11-28 17:16:49 -0300

    feat:prod_stuff
    Changed and added some stuff to adapt the project to the server
    
    Lucas F. A. Rocha

--- frontend/Estoque_WebApp/src/environments/environment.prod.ts
+++ frontend/Estoque_WebApp/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,  
     // API_URL: "http://192.168.1.75:8080/api",
-    API_URL: "http://localhost:8080/api",
+    API_URL: "/api",
     TOKEN_KEY: "Centro_Paula_Souza_Token"
 };
